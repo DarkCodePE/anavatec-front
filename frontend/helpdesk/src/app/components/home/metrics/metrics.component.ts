0ccc43518615a79bfe3665c2fbe7ab04521a316e
Orlando Kuan BecerrA <orlandokuanb@gmail.com>
2023-10-17 10:45:25 -0500

    REFACTOR: ADD SOLUTION MODULE --REV5

---
+++ frontend/helpdesk/src/app/components/home/metrics/metrics.component.ts
@@ -0,0 +1,162 @@
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {
+  ChartComponent,
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexFill,
+  ApexTooltip,
+  ApexXAxis,
+  ApexLegend,
+  ApexDataLabels,
+  ApexTitleSubtitle,
+  ApexYAxis
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  markers: any; //ApexMarkers;
+  stroke: any; //ApexStroke;
+  yaxis: ApexYAxis | ApexYAxis[];
+  dataLabels: ApexDataLabels;
+  title: ApexTitleSubtitle;
+  legend: ApexLegend;
+  fill: ApexFill;
+  tooltip: ApexTooltip;
+};
+
+@Component({
+  selector: 'app-metrics',
+  templateUrl: './metrics.component.html',
+  styleUrls: ['./metrics.component.css']
+})
+export class MetricsComponent implements OnInit {
+  @ViewChild("chart") chart: ChartComponent;
+  public chartOptions: Partial<ChartOptions>;
+  constructor() {
+    this.chartOptions = {
+      series: [
+        {
+          name: "Ingreso",
+          type: "column",
+          data: [1.4, 2, 2.5, 1.5, 2.5, 2.8, 3.8, 4.6]
+        },
+        {
+          name: "Fondos",
+          type: "column",
+          data: [1.1, 3, 3.1, 4, 4.1, 4.9, 6.5, 8.5]
+        },
+        {
+          name: "Ganancia",
+          type: "line",
+          data: [20, 29, 37, 36, 44, 45, 50, 58]
+        }
+      ],
+      chart: {
+        height: 350,
+        type: "line",
+        stacked: false
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        width: [1, 1, 4]
+      },
+      title: {
+        text: "Análisis de costos (2009 - 2016)",
+        align: "left",
+        offsetX: 110
+      },
+      xaxis: {
+        categories: [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016]
+      },
+      yaxis: [
+        {
+          axisTicks: {
+            show: true
+          },
+          axisBorder: {
+            show: true,
+            color: "#008FFB"
+          },
+          labels: {
+            style: {
+              colors: "#008FFB"
+            }
+          },
+          title: {
+            text: "Ingresos",
+            style: {
+              color: "#008FFB"
+            }
+          },
+          tooltip: {
+            enabled: true
+          }
+        },
+        {
+          seriesName: "Income",
+          opposite: true,
+          axisTicks: {
+            show: true
+          },
+          axisBorder: {
+            show: true,
+            color: "#00E396"
+          },
+          labels: {
+            style: {
+              colors: "#00E396"
+            }
+          },
+          title: {
+            text: "Flujo de caja",
+            style: {
+              color: "#00E396"
+            }
+          }
+        },
+        {
+          seriesName: "Revenue",
+          opposite: true,
+          axisTicks: {
+            show: true
+          },
+          axisBorder: {
+            show: true,
+            color: "#FEB019"
+          },
+          labels: {
+            style: {
+              colors: "#FEB019"
+            }
+          },
+          title: {
+            text: "Gastos",
+            style: {
+              color: "#FEB019"
+            }
+          }
+        }
+      ],
+      tooltip: {
+        fixed: {
+          enabled: true,
+          position: "topLeft", // topRight, topLeft, bottomRight, bottomLeft
+          offsetY: 30,
+          offsetX: 60
+        }
+      },
+      legend: {
+        horizontalAlign: "left",
+        offsetX: 40
+      }
+    };
+  }
+
+  ngOnInit(): void {
+  }
+
+}
